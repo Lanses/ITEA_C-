9ef6d12cb3f03d87d7b9b7c4c4d363f1c121edb5
Lanses <chevpilov@gmail.com>
2019-04-01 14:47:59 +0300

    Commit_ v3

--- Homework_1_v1/Calculating_2/Program.cs
+++ Homework_1_v1/Calculating_2/Program.cs
@@ -6,7 +6,27 @@ namespace Calculating_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Result();
+            Console.ReadLine();
+            Console.ReadLine();
+            Console.ReadLine();
         }
+
+
+        public static void Result()
+        {
+            var A = int.Parse(Console.ReadLine());
+            var B = int.Parse(Console.ReadLine());
+            var C = int.Parse(Console.ReadLine());
+            if (A > C)
+            {
+                Console.WriteLine((A + B) / 5 + Math.Pow(Math.Sin(C), 2));
+            }
+            else
+            {
+                Console.WriteLine(Math.Sqrt(Math.Abs(B + (Math.Pow(Math.Cos(C), 2)))));
+            }
+        }
+
     }
 }
