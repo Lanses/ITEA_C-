b8ed7b90708b1e8160a117540e2e81dda9277a6d
Lanses <chevpilov@gmail.com>
2019-04-01 14:30:43 +0300

    Commit_v2

---
+++ Homework_1_v1/Calculating_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Calculating1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Result();
+            Console.ReadLine();
+            Console.ReadLine();
+
+        }
+
+        public static void Result()
+        {
+            var A = int.Parse(Console.ReadLine());
+            var B = int.Parse(Console.ReadLine());
+            if (A >= B)
+            {
+                Console.WriteLine((A - B / 2) / (2 * Math.Pow(A + B, 2)));
+            }
+            else
+            {
+                Console.WriteLine(Math.Sqrt(A + Math.Pow(B + 1, 2)));
+            }
+        }
+
+    }
+}
---
+++ Homework_1_v1/Fibonacci_Recursion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fibonacci_Recursion
+{
+    class Program
+    {
+        public static double Fibonacci_Recursion(int x)
+        {
+            int a = 0, b = 1, c = 0;
+            Console.Write("{0} \n {1} \n", a, b);
+            while (x != 1) {
+                c = a + b;
+                Console.Write(" {0}", c);
+                Console.Write("\n");
+                a = b;
+                b = c;
+                x = x - 1;
+            }
+            return c;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            //Console.ReadLine();
+            int x;
+            x = int.Parse(Console.ReadLine());
+            Fibonacci_Recursion(x);
+
+        }
+    }
+}
