05564649fc11a280bc4538d19f697fb17751d560
Lanses <chevpilov@gmail.com>
2019-04-02 21:01:51 +0300

    v2

---
+++ Just_Practise/Array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int a = 0;
+            for (int i = k; i <= n; i++)
+            {
+
+                if (i % 2 != 0)
+                {
+                    a += i;
+                }
+            }
+            Console.WriteLine(a);
+
+            Console.WriteLine("\n");
+
+            int sum = 0;
+            int[] arr = new int[100];
+            Random rnd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int value = rnd.Next(-100, 101);
+                arr[i] = value;
+                sum += value;
+            }
+            double max = arr.Max();
+            double min = arr.Min();
+            double middle = sum / arr.Length;
+
+            Console.WriteLine(min);
+            Console.WriteLine("\n");
+            Console.WriteLine(max);
+            Console.WriteLine("\n");
+            Console.WriteLine(sum);
+        }
+    }
+}
--- Just_Practise/Just_Practise/Program.cs
+++ Just_Practise/Just_Practise/Program.cs
@@ -6,7 +6,42 @@ namespace Just_Practise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //var i = 0;
+            for (int i = 0; i < 10 ; i++) {
+                Console.WriteLine("Hello World!");
+            }
+
+            Console.WriteLine("\n");
+
+
+            int n = 0;
+            while (n < 10)
+            {
+                Console.WriteLine("Hello World!");
+                n++;
+            }
+
+
+            Console.WriteLine("\n");
+
+
+            int a = 0;
+            do
+            {
+                Console.WriteLine("Hello World!");
+                a++;
+            }
+            while (a < 10);
+
+
+            /*for (int i = 0; ; i++)
+            {
+                    Console.WriteLine("Hello World!");
+                }
+            }*/
+
+            
+
         }
     }
 }
