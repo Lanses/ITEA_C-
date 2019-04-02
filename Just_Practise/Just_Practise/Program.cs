1898bd4cb7122d4cfbaa46816c5187ec5bb16274
Lanses <chevpilov@gmail.com>
2019-04-02 19:08:53 +0300

    v1

---
+++ Just_Practise/Just_Practise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Just_Practise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
