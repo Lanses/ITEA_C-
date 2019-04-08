067050119360b83a1889c999749cff6197e5fb65
Lanses <chevpilov@gmail.com>
2019-04-08 16:56:14 +0300

    For_checking_v1

--- TravelProject/TravelProject/DataBase.cs
+++ TravelProject/TravelProject/DataBase.cs
@@ -6,19 +6,34 @@ namespace TravelProject
 {
     public class DataBase
     {
-        public string[] DepartureCountry = new string[] {"Ukraine",  "Poland", "Germany", "France", "Austria", "Sweeden"};
-        public string[] DepartureCity = new string[] {"Kyiv", "Wroclav", "Hamburg", "Paris", "Vienna", "Stockholm" };
-        public string[] DestinationCountry = new string[] { "Ukraine", "Poland", "Germany", "France", "Austria", "Sweeden" };
-        public string[] DestinationCity = new string[] {"Kyiv", "Wroclav", "Hamburg", "Paris", "Vienna", "Stockholm" };
+        public string[] DepartureCountry = new string[]
+        {
+            "Ukraine",  "Poland", "Germany", "France", "Austria", "Sweeden"
+        };
+        public string[] DepartureCity = new string[]
+        {
+            "Kyiv", "Wroclav", "Hamburg", "Paris", "Vienna", "Stockholm"
+        };
+        public string[] DestinationCountry = new string[]
+        {
+            "Ukraine", "Poland", "Germany", "France", "Austria", "Sweeden"
+        };
+        public string[] DestinationCity = new string[]
+        {
+            "Kyiv", "Wroclav", "Hamburg", "Paris", "Vienna", "Stockholm"
+        };
 
 
-        public int _price;
-        public int _numberOfDays;
-        public int _numberOfPersons;
-        public DateTime _date;
+
+        public int _numberOfDays { get; set; }
+        public int _numberOfPersons { get; set; }
+        public DateTime _date { get; set; }
 
 
-        
+        public void Price()
+        {
+
+        }
 
     }
 }
