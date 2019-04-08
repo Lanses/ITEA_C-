7430ce7f5a144663fade7f711f092629200fe333
Mykhailo <budda_2@mail.ru>
2019-04-08 22:05:24 +0300

    Add Travel Project and prepare for review
    This reverts commit e0e5ea4a22a4ca65998daf3bbad1e65e3a7cc867.

---
+++ TravelProject/TravelProject/Search.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelProject
+{
+    public class Search : DataBase 
+    {
+        public Search(string departureCountry, string departureCity, string destinationCountry, string destinationCity)
+        {
+            DepartureCountry = departureCountry;
+            DepartureCity = departureCity;
+            DestinationCountry = destinationCountry;
+            DestinationCity = destinationCity;
+        }
+    }
+}
