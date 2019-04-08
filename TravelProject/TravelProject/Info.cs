d2f2236e7660ee144ee4fda3f3b446dff1eaa224
Mykhailo <budda_2@mail.ru>
2019-04-08 22:14:56 +0300

    Revert "Remove TravelProject"
    This reverts commit e0e5ea4a22a4ca65998daf3bbad1e65e3a7cc867.

---
+++ TravelProject/TravelProject/Info.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelProject
+{
+    public class Info : DataBase
+    {
+
+        public string _location;
+        public string _phone;
+        
+    }
+}
